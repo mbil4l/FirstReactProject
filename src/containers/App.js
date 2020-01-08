5d26d35e3448262ff8677cab660b44de2b908753
badassbilal <mbilalkhan500@gmail.com>
2020-01-08 17:58:59 +0500

    Completed. Code just needs to be Optimized

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { robots } from '../robots';
+import CardList from '../components/CardList';
+import SearchBar from '../components/SearchBar';
+import Scroll from '../components/Scroll';
+
+class App extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    OnSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    //console.log(filteredRobots);    
+    }
+
+    render() {
+    const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot =>  {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (!robots.length){
+        return <h1 className='title grow tc shadow-5 f-headline'>Loading</h1>
+    }
+    return (
+    <div className='tc'>
+        <h1 className='title grow shadow-5 f-headline'> Robots </h1>
+        <SearchBar searchChange={this.OnSearchChange}/>
+        <scroll>
+            <CardList robots={filteredRobots}/>
+        </scroll>
+    </div>)  
+}}
+
+export default App;
+
+
+
+  
